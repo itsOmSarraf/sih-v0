1efe902557a9ab827fdec06c67ef17f077012a83
Om Sarraf <itsomsarraf+github@gmail.com>
2024-08-12 12:28:02 +0530

    protected paths

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,19 @@
-export { auth as middleware } from '@/lib/auth';
-// Don't invoke Middleware on some paths
+import { auth } from '@/lib/auth';
+
+const protectedRoutes = ['/dashboard', '/collection'];
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isProtectedRoute = protectedRoutes.includes(req.nextUrl.pathname);
+
+  if (isProtectedRoute && !isLoggedIn) {
+    return Response.redirect(new URL('/api/auth/signin', req.url));
+  }
+});
+
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: [
+    ...protectedRoutes,
+    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+  ]
 };
