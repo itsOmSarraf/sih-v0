93f88643d0e583f975df48bf59708705dc19c220
Om Sarraf <itsomsarraf+github@gmail.com>
2024-08-18 20:05:12 +0530

    new schema

--- migrations/0000_odd_proemial_gods.sql
+++ migrations/0000_odd_proemial_gods.sql
@@ -1,7 +1,8 @@
 CREATE SCHEMA "my_schema";
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "my_schema"."event" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"singer_username" text NOT NULL,
+	"event_number" integer NOT NULL,
 	"name" text NOT NULL,
 	"description" text,
 	"eventDate" date NOT NULL,
@@ -9,13 +10,13 @@ CREATE TABLE IF NOT EXISTS "my_schema"."event" (
 	"venue" text NOT NULL,
 	"ticketLink" text,
 	"image" text,
-	"singer_id" integer NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "event_singer_username_event_number_pk" PRIMARY KEY("singer_username","event_number")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "my_schema"."singer" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"username" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"gender" char(1),
 	"age" integer,
@@ -24,6 +25,7 @@ CREATE TABLE IF NOT EXISTS "my_schema"."singer" (
 	"contactNo" varchar(15),
 	"pfp" text,
 	"upi_id" text,
+	"event_count" integer DEFAULT 0 NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "singer_email_unique" UNIQUE("email")
@@ -31,7 +33,8 @@ CREATE TABLE IF NOT EXISTS "my_schema"."singer" (
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "my_schema"."song_request" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"event_id" integer NOT NULL,
+	"singer_username" text NOT NULL,
+	"event_number" integer NOT NULL,
 	"phone" varchar(15) NOT NULL,
 	"name" text NOT NULL,
 	"dedicated_to" text,
@@ -43,13 +46,7 @@ CREATE TABLE IF NOT EXISTS "my_schema"."song_request" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "my_schema"."event" ADD CONSTRAINT "event_singer_id_singer_id_fk" FOREIGN KEY ("singer_id") REFERENCES "my_schema"."singer"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "my_schema"."song_request" ADD CONSTRAINT "song_request_event_id_event_id_fk" FOREIGN KEY ("event_id") REFERENCES "my_schema"."event"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "my_schema"."event" ADD CONSTRAINT "event_singer_username_singer_username_fk" FOREIGN KEY ("singer_username") REFERENCES "my_schema"."singer"("username") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- migrations/0001_wonderful_doctor_octopus.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "my_schema"."singer" ADD COLUMN "username" text;--> statement-breakpoint
-ALTER TABLE "my_schema"."singer" ADD CONSTRAINT "singer_username_unique" UNIQUE("username");
