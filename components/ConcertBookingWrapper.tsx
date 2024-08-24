ea27a0302afdd7e84143e9b71ba499d50b5efd49
Om Sarraf <itsomsarraf+github@gmail.com>
2024-08-25 02:51:21 +0530

    profile

--- components/ConcertBookingWrapper.tsx
+++ components/ConcertBookingWrapper.tsx
@@ -1,9 +1,13 @@
-
 import { ProfileData } from 'app/actions/profile';
 import ConcertBookingForm from './eventForm';
 
 export default async function ConcertBookingWrapper() {
     const profile = await ProfileData();
+
+    if (!profile) {
+        // Handle the case where no profile data is available
+        return <div>Please log in to book a concert.</div>;
+    }
 
     return <ConcertBookingForm profile={profile} />;
 }
