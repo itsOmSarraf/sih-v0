7f16b3add8a290ab44875372460e68dd737328e8
Om Sarraf <itsomsarraf+github@gmail.com>
2024-08-25 02:38:46 +0530

    navabr avatar fixes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { Menu } from 'lucide-react';
 import Link from 'next/link';
@@ -14,21 +15,26 @@ import { APP_NAME } from '@/lib/constants';
 import handleLogout from '@/lib/signout';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { ProfileData } from 'app/actions/profile';
+
 export default async function NavBar() {
   const profileData = await ProfileData();
+  const isLoggedIn = profileData && profileData.userName && profileData.name;
+
   return (
     <nav className="flex justify-between text-primary items-center p-4 bg-white">
       <Link href="/">{APP_NAME}</Link>
       <div className="flex gap-2">
-        <Link href={`/singer/${profileData.userName}`}>
-          <Avatar>
-            <AvatarImage
-              src={profileData.pfp}
-              alt={`${profileData.name}'s profile picture`}
-            />
-            <AvatarFallback>{profileData.name.charAt(0)}</AvatarFallback>
-          </Avatar>
-        </Link>
+        {isLoggedIn && (
+          <Link href={`/singer/${profileData.userName}`}>
+            <Avatar>
+              <AvatarImage
+                src={profileData.pfp}
+                alt={`${profileData.name}'s profile picture`}
+              />
+              <AvatarFallback>{profileData.name.charAt(0)}</AvatarFallback>
+            </Avatar>
+          </Link>
+        )}
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
@@ -41,43 +47,47 @@ export default async function NavBar() {
               <SheetDescription>Navigate through our site</SheetDescription>
             </SheetHeader>
             <div className="mt-4">
-              <Button onClick={handleLogout}>Logout</Button>
-              {
-                <ul className="space-y-2">
-                  <li>
-                    <Link
-                      href="#"
-                      className="block p-2 hover:bg-gray-200 rounded"
-                    >
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="block p-2 hover:bg-gray-200 rounded"
-                    >
-                      About
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="block p-2 hover:bg-gray-200 rounded"
-                    >
-                      Services
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="block p-2 hover:bg-gray-200 rounded"
-                    >
-                      Contact
-                    </Link>
-                  </li>
-                </ul>
-              }
+              {isLoggedIn ? (
+                <Button onClick={handleLogout}>Logout</Button>
+              ) : (
+                <Link href="/login">
+                  <Button>Login</Button>
+                </Link>
+              )}
+              <ul className="space-y-2 mt-4">
+                <li>
+                  <Link
+                    href="/"
+                    className="block p-2 hover:bg-gray-200 rounded"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/about"
+                    className="block p-2 hover:bg-gray-200 rounded"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services"
+                    className="block p-2 hover:bg-gray-200 rounded"
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/contact"
+                    className="block p-2 hover:bg-gray-200 rounded"
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
             </div>
           </SheetContent>
         </Sheet>
