d3b2d65d316fd33a1a4e214d68490c9fec7285ff
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-09-01 08:10:55 +0530

    landing made responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex items-center justify-between px-6">
-      <div className="flex items-center">
+    <div className="flex items-center justify-between px-6 pt-4 md:pt-0 h-16 md:h-auto">
+      <div className="hidden md:flex items-center">
         <img src="/emblem.png" alt="Emblem" className="w-14 h-20" />
         <img src="/logo.png" alt="Logo" className="w-24" />
         <img
@@ -21,20 +21,26 @@ const Navbar = () => {
         />
       </div>
       <div>
-        <h1 className="text-[#75002b] text-5xl font-bold">
-          {multiLang[language].NavbarTitle}
-        </h1>
-        <p className="text-gray-500">{multiLang[language].NavbarSubTitle}</p>
+        <a href="/">
+          <h1 className="text-[#75002b] text-2xl md:text-5xl font-bold">
+            {multiLang[language].NavbarTitle}
+          </h1>
+        </a>
+        <p className="text-gray-500 hidden md:block">
+          {multiLang[language].NavbarSubTitle}
+        </p>
       </div>
       <div className="flex items-center gap-5">
-        <div className="cursor-pointer flex items-center gap-1 bg-orange-500 text-3xl text-white font-bold px-4 py-2 rounded-lg">
+        <div className="cursor-pointer flex items-center gap-1 bg-orange-500  md:text-3xl text-white font-bold px-2 md:px-4 py-1 md:py-2 rounded-lg">
           <Phone />
           <a href="tel:139">139</a>
         </div>
-        <p className="text-base">{multiLang[language].NavbarSubTitle2}</p>
+        <p className="text-base hidden md:block">
+          {multiLang[language].NavbarSubTitle2}
+        </p>
       </div>
-      <div className="flex gap-3">
-        <button className="bg-[#dcdef9] px-8 py-2 rounded-lg">
+      <div className=" gap-3 hidden md:flex">
+        <button className="bg-[#dcdef9] px-8 py-2 rounded-lg ">
           {multiLang[language].NavbarFAQ}
         </button>
         <button className="bg-[#efe4e8] px-6 py-2 rounded-lg">
@@ -47,7 +53,7 @@ const Navbar = () => {
           id="language"
           value={language} // Bind the select value to the global language state
           onChange={handleLanguageChange} // Update the global state on change
-          className="border-[1px] border-gray-300 rounded-lg px-5 py-2"
+          className="border-[1px] border-gray-300 rounded-lg px-2 py-1"
         >
           <option value="en">English</option>
           <option value="hi">हिन्दी</option>
